cb933007215ce92f6043c6fe483f4df0f4defb53
unknown <you@example.com>
2024-12-26 22:53:39 +0700

    Done Two Sum Using hash map

---
+++ Twosum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> hash;
+    for(int i = 0 ; i < nums.size() ; i++){
+        if (hash[nums[i]] != 0){
+            if (hash[nums[i]] == -1){
+                return {0,i};
+            }
+            return {hash[nums[i]],i};
+        }
+        hash[target - nums[i]] = (i == 0)? -1 :i;
+        }
+    return {-69,-420};
+    }
+};
